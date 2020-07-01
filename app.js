22c113560058d4ed12081926d46a20f920129faf
Antti Rintala <rintsi@gmail.com>
2020-07-01 15:51:29 +0300

    Add operation result printer

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 'use strict';
 
 const grpc = require('grpc');
-//const mqtt = require('azure-iot-device-mqtt').Mqtt;
-//const ModuleClient = require('azure-iot-device').ModuleClient;
-//const DeviceClient = require('azure-iot-device').Client;
-//const Message = require('azure-iot-device').Message;
+const mqtt = require('azure-iot-device-mqtt').Mqtt;
+const ModuleClient = require('azure-iot-device').ModuleClient;
+const Message = require('azure-iot-device').Message;
 const proto = require('@ceruleandatahub/proto');
+
+const OUTPUT_EVENT_NAMESPACE = process.env.OUTPUT_EVENT_NAMESPACE;
+const GRPC_ADDRESS = process.env.GRPC_ADDRESS;
 
 function createGRPCServer() {
   console.log('Starting gRCP Server');
@@ -14,36 +16,17 @@ function createGRPCServer() {
   server.addService(proto.CurrentLoopProto.CurrentLoop.service, {
     sendTelemetry: sendTelemetry
   });
-  server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
+  server.bind(GRPC_ADDRESS, grpc.ServerCredentials.createInsecure());
   server.start();
 }
 
-createGRPCServer();
-
-/*let moduleClient;
+let moduleClient;
 ModuleClient.fromEnvironment(mqtt, function (err, client) {
   if (err) {
     throw err;
   } else {
     client.on('error', function (err) {
       throw err;
-    });
-
-    client.onMethod(DEVICE_REGISTRATION_METHOD_NAME, (request, response) => {
-      console.log(`${DEVICE_REGISTRATION_METHOD_NAME} method called`, request);
-      handleDeviceRegistrationMessage(request.payload);
-      response.send(200, '', function (err) {
-        if (err) {
-          console.log(
-            `Error sending response to ${DEVICE_REGISTRATION_METHOD_NAME} method`,
-            err
-          );
-        } else {
-          console.log(
-            `Response to ${DEVICE_REGISTRATION_METHOD_NAME} method sent successfully`
-          );
-        }
-      });
     });
 
     client.open(function (err) {
@@ -56,22 +39,33 @@ ModuleClient.fromEnvironment(mqtt, function (err, client) {
       }
     });
   }
-});*/
+});
+
+const printResultFor = op => {
+  return (err, res) => {
+    if (err) console.log(op + ' error: ' + err.toString());
+    if (res) console.log(op + ' status: ' + res.constructor.name);
+  };
+};
 
 function sendTelemetry(call, callback) {
-  console.log(call.request);
+  const message = new Message(JSON.stringify(call.request));
 
-  /*const msg = new Message(JSON.stringify(call.request));
-
-  msg.properties.add('type', 'telemetry');
-  msg.properties.add(
+  message.properties.add('type', 'telemetry');
+  /*message.properties.add(
     'level',
     messageLevelHandler.resolveLevel(
       telemetryData,
       alreadyDiscoveredDevice.deviceTwin
     )
+  );*/
+  console.log('Send message', JSON.stringify(message));
+  moduleClient.sendEvent(message, printResultFor('Sending event to upstream'));
+  moduleClient.sendOutputEvent(
+    OUTPUT_EVENT_NAMESPACE,
+    message,
+    printResultFor(`Sending received message to ${OUTPUT_EVENT_NAMESPACE}`)
   );
-  console.log('Send msg', JSON.stringify(msg));
-  moduleClient.sendEvent(msg, printResultFor('send'));*/
+
   callback(null, {hash: call.request.hash});
 }
